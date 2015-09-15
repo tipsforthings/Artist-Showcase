ebcdd2e8f642db9cc1cba7f9a46c24214306e885
Alex Scott <alex.scott@tipsforthings.com>
2015-09-15 23:45:05 +0100

    Setup animate in header and customizer

--- header.php
+++ header.php
@@ -25,11 +25,11 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a class="animated rubberBand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title <?php if ( get_theme_mod('header_animate') == true ) { ?>animated <?php echo get_theme_mod('title_animation', 'rubberBand'); } ?> "><a class="animated swing" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
-				<p class="site-title"><a class="animated rubberBand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title <?php if ( get_theme_mod('header_animate') == true ) { ?>animated <?php echo get_theme_mod('title_animation', 'rubberBand'); } ?>"><a class="animated rubberBand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php endif; ?>
-			<div class="site-description animated fadeIn"><?php bloginfo( 'description' ); ?></div>
+			<div class="site-description <?php if ( get_theme_mod('header_animate') == true ) { ?>animated <?php echo get_theme_mod('tagline_animation', 'rubberBand'); } ?>"><?php bloginfo( 'description' ); ?></div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready( function() {
+
+  var $count = $("#primary-menu").children().length;
+  var $linkwidth = 100/$count;
+
+  $(".menu-item").css({'width': $linkwidth + '%'});
+});
